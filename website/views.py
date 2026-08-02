9cecba97a88fd8bc0e22493cec06d1de358ee7ae
Zeinab-Safinia <safiniazeinab@gmail.com>
2026-08-03 00:42:54 +0330

    Move templates directory to project root
    refactor: move templates directory to project root

--- website/views.py
+++ website/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return render(request, 'web/index.html')
+    return render(request, 'website/index.html')
 def about_view(request):
-    return render(request, 'web/about.html')
+    return render(request, 'website/about.html')
 def contact_view(request):
-    return render(request, 'web/contact.html')
+    return render(request, 'website/contact.html')
