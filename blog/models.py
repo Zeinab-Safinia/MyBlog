587e37ea0ba76ee109dd228d6c70bae3d601eec7
Zeinab-Safinia <safiniazeinab@gmail.com>
2026-08-21 11:18:48 +0330

    created Meta class

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,14 @@ class Post(models.Model):
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    
+    
+    class Meta:
+        ordering = ['created_date']
+        # verbose_name = 'پست'
+        # verbose_name_plural = 'پست ها'
+        
+        
     def __str__(self):
       return self.title
     
