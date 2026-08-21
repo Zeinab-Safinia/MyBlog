7cf69330b5294d570a3c93c50b47aa6eb953a41b
Zeinab-Safinia <safiniazeinab@gmail.com>
2026-08-21 11:43:20 +0330

    completed Contact register

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from website.models import Contact
 # Register your models here.
+
+
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name','subject','created_date')
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    ordering = ['created_date']
+    search_fields = ['name']
+    list_filter = ('email',)
+    
+admin.site.register(Contact,ContactAdmin)
