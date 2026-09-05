c3dcfd78571fb0543be5a6d0ebb098df6acf5b39
Zeinab-Safinia <safiniazeinab@gmail.com>
2026-09-05 13:07:56 +0330

    handelling status for showing posts

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ def blog_view(request):
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html',context)
 def blog_single(request,pid):
-    post = get_object_or_404(Post,pk=pid)
+    post = get_object_or_404(Post,pk=pid,status=1)
     context = {'post':post}
     return render(request, 'blog/blog-single.html',context)
 
