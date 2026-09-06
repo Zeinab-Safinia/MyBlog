a9044ab43120a56c603ca6b199328583d6a487dd
Zeinab-Safinia <safiniazeinab@gmail.com>
2026-09-06 23:10:08 +0330

    add image field

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    # image
+    image = models.ImageField(upload_to='blog/',default='blog\defalt.jpg')
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
