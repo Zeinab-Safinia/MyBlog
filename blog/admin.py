b84a6091ba4b96cfeec78aa29d1300845a62cea0
Zeinab-Safinia <safiniazeinab@gmail.com>
2026-09-06 20:43:23 +0330

    Add author field

--- blog/admin.py
+++ blog/admin.py
@@ -8,8 +8,8 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
     # fields = ('title',)
-    list_display = ('id','title','counted_view','status','published_date','created_date')
-    list_filter = ('status',)
+    list_display = ('id','title','author','counted_view','status','published_date','created_date')
+    list_filter = ('status','author')
     ordering = ['created_date']
     search_fields = ['title','content']
 admin.site.register(Post,PostAdmin)
